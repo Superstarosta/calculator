73bf1e538a52c961d14f833d5e730393b4561e29
Superstarosta <sinjagin2009@yandex.ru>
2016-04-06 23:54:28 +0300

    библиотека функций калькулятора

---
+++ Calculating.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Calculating
+    {
+        public static double Addition(double firstArgument, double secondArgument)
+        {
+            return firstArgument + secondArgument;
+        }
+
+        public static double Substraction(double firstArgument, double secondArgument)
+        {
+            return firstArgument - secondArgument;
+        }
+
+        public static double Multiplication(double firstArgument, double secondArgument)
+        {
+            return firstArgument * secondArgument;
+        }
+
+        public static double Division(double firstArgument, double secondArgument)
+        {
+            if((secondArgument-0)<double.Epsilon)
+                throw new Exception("Division to zero is bad\n");
+
+            return firstArgument / secondArgument;
+        }
+
+        public static double Mod(double firstArgument, double secondArgument)
+        {
+            return firstArgument % secondArgument;
+        }
+    }
+}
